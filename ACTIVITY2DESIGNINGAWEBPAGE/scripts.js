b35b5ba495e9af2f4d12486c0b16266941d957c1
Alan Adamson <151865658+Agauna13@users.noreply.github.com>
2024-10-04 08:38:27 +0200

    wireframe casi listo

---
+++ ACTIVITY2DESIGNINGAWEBPAGE/scripts.js
@@ -0,0 +1,35 @@
+function showSideBar(){
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'flex';
+
+}
+
+function hideSideBar(){
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'none';
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("diapositiva");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
